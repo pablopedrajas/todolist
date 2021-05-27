d67ac20a0a6ca511d13bb03becb2aacf3f885919
licensede <pablped@gmail.com>
2021-05-27 18:33:04 +0200

    adding modules date.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const date = require(__dirname + "/date.js")
 
 const app = express()
 
@@ -13,15 +14,8 @@ let items = ["Buy food", "Cook Food", "Eat Food"];
 let workItems = []
 
 app.get('/', (req, res) => {
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric", 
-        month: "long"
-    }
-
-    let day = today.toLocaleDateString("en-US", options)
+    
+    let day = date()
 
     res.render("list", { 
         listTitle: day,
